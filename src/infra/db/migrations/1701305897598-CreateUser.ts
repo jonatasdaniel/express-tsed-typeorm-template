98b59bfdc246497beacdaed8bd32602ed1cbb20d
Jonatas Daniel Hermann <jonatas.hermann@gmail.com>
2023-11-29 21:59:45 -0300

    feat: Add uuid and user migrations

---
+++ src/infra/db/migrations/1701305897598-CreateUser.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUser1701305897598 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            isUnique: true,
+            generationStrategy: "uuid",
+            default: `uuid_generate_v4()`,
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "encrypted_password",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
